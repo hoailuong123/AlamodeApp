7e7bef7e993cd3f11adedd132c686407fb40a126
HoaiLuong <hoailuong160202@gmail.com>
2024-10-14 03:34:35 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
+// import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'core/app_export.dart';
-
 var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
 
 void main() {
