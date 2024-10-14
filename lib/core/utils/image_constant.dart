ac13e32a66562aed14721bd5b9d4018ba6bcf6d0
HoaiLuong <hoailuong160202@gmail.com>
2024-10-14 20:49:04 +0700

    add utils

--- lib/core/utils/image_constant.dart
+++ lib/core/utils/image_constant.dart
@@ -1,5 +1,6 @@
 class ImageConstant {
   static String imagePath = 'assets/images';
-  static String imageLogin = '$imagePath/login.png';
+  static String imgLogin = '$imagePath/login.png';
   static String iconHeart = '$imagePath/heart.png';
+  static String imgCheckMark = '$imagePath/img_checkmark.svg';
 }
