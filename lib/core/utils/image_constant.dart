abc489429b00688981b040dda6f24c36bc1a1203
HoaiLuong <hoailuong160202@gmail.com>
2024-10-17 10:11:35 +0700

    add constant

--- lib/core/utils/image_constant.dart
+++ lib/core/utils/image_constant.dart
@@ -3,4 +3,7 @@ class ImageConstant {
   static String imgLogin = '$imagePath/login.png';
   static String iconHeart = '$imagePath/heart.png';
   static String imgCheckMark = '$imagePath/img_checkmark.svg';
+  static String imgBag = '$imagePath/imageBag.png';
+  static String iconCam = '$imagePath/Icon Camera.png';
+
 }
