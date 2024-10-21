f9ce99335cc519c4229dd82c5d71907229735e1d
DatPN <datpn@outfiz.com>
2024-10-21 23:29:11 +0700

    image create-account-screen

--- lib/core/utils/image_constant.dart
+++ lib/core/utils/image_constant.dart
@@ -4,5 +4,9 @@ class ImageConstant {
   static String iconHeart = '$imagePath/heart.png';
   static String iconBtnLeft = '$imagePath/button_left.png';
   static String iconBag = '$imagePath/img_bag.png';
-
+  static String imgFlag = '$imagePath/img_flag.png';
+  static String imgPw = '$imagePath/img_password.png';
+  static String imageBgCreateAccount = '$imagePath/img_bg_create_account.png';
+  static String imgCamera = '$imagePath/img_camera.png';
+  
 }
